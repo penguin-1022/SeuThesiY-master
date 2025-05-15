6fbaf1725636f0e8508397f80149464cbd0c8d9a
Jensen Jia <penguin-1022@qq.com>
2025-05-15 15:13:06 +0800

    20250515-1

--- figures/question_ratio.py
+++ figures/question_ratio.py
@@ -5,7 +5,7 @@ import numpy as np
 matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体
 matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号 '-' 显示问题
 # 问题类型和对应占比
-labels = ['位置类问题', '位置关系类问题', '与位置有关的计数类问题']
+labels = ['位置类问题', '位置关系类问题', '与位置相关的计数类问题']
 sizes = [33.9, 32.8, 33.3]
 colors = ['#66c2a5', '#fc8d62', '#8da0cb']
 
